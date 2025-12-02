acef7b66c119366cac65da80c362df50833fae8e
singhwillcode <dimondcartofficial@gmail.com>
2025-12-02 21:34:10 +0530

    Updated: added TimeBased code

--- necessary codes/daoContract.sol/daoContract.sol/voting_system/oops concepts/inheritancep2.sol
+++
@@ -1,8 +0,0 @@
-
-pragma solidity >= 0.7.0;
-
-    import "./inheritancepart1.sol";
-    contract child is owner{
-
-    } 
-            
--- necessary codes/daoContract.sol/daoContract.sol/voting_system/oops concepts/inheritancepart1.sol
+++
@@ -1,9 +0,0 @@
- // SPDX-License-Identifier: MIT
- pragma solidity >=0.7.0;
-
-    contract owner{
-        function add(uint a , uint b ) public pure returns(uint)
-        {
-            return a+b;
-        }
-    }
